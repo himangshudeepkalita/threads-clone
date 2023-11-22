07eb347ad7f1ddeb8eaa8be051e84a66bb6b2a6b
itzhimangshu <itzhimangshu@gmail.com>
2023-11-22 20:44:27 +0530

    ui design

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import { Button, Flex, Image, Link, useColorMode } from "@chakra-ui/react";
+
+const Header = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <Flex justifyContent={"space-between"} mt={6} mb="12">
+      <Image
+        cursor={"pointer"}
+        alt="logo"
+        w={6}
+        src={colorMode === "dark" ? "/light-logo.svg" : "/dark-logo.svg"}
+        onClick={toggleColorMode}
+      />
+    </Flex>
+  );
+};
+
+export default Header;
