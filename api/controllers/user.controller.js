4e95084da44c8672318b19468657c7a777bed2b2
itzhimangshu <itzhimangshu@gmail.com>
2023-12-01 13:34:08 +0530

    rest api

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -53,3 +53,72 @@ export const login = async (req, res, next) => {
         next(error);
     }
 }
+
+export const logout = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token')
+    res.status(200).json('User logged out successfully!!!!')
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const followUnfollowUser = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const userToModify = await User.findById(id);
+    const currentUser = await User.findById(req.user.id);
+
+    // console.log('ID:', id);
+    // console.log('User to Modify:', userToModify);
+    // console.log('Current User:', currentUser);
+
+    if (id === req.user.id.toString()) return res.status(400).json({ message: 'You cannot follow/unfollow yourself!!!!'});
+
+    if (!userToModify || !currentUser) return res.status(400).json({ message: 'User not found!!!'});
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      // Unfollow user
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user.id } });
+      await User.findByIdAndUpdate(req.user.id, { $pull: { following: id } });
+      res.status(200).json({ message: 'User unfollowed successfully!!!' });
+    } else {
+      // Follow user
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user.id } });
+      await User.findByIdAndUpdate(req.user.id, { $push: { following: id } });
+      res.status(200).json({ message: 'User followed successfully!!!' });
+    }
+  } catch (error) {
+    next(error);
+  }
+}
+
+export const updateUser = async (req, res, next) => {
+  const { name, email, username, password, profilePic, bio } = req.body;
+  const userId = req.user.id;
+  try {
+    let user = await User.findById(userId);
+    if (!user) return res.status(400).json({ message: 'User not found!!!' });
+
+    if(req.params.id !== userId.toString()) return res.status(400).json({ message: "You cannot update other user's profile" });
+
+    if(password) {
+      const hashedPassword = bcrypt.hashSync(password, 10);
+      user.password = hashedPassword;
+    }
+
+    user.name = name || user.name;
+    user.email = email || user.email;
+    user.username = username || user.username;
+    user.profilePic = profilePic || user.profilePic;
+    user.bio = bio || user.bio;
+
+    user = await user.save();
+
+    res.status(200).json({ message: 'Profile updated successfully!!!', user });
+  } catch (error) {
+    next(error);
+  }
+}
--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,9 +1,14 @@
 import express from 'express';
-import { login, signup } from '../controllers/user.controller.js';
+import { followUnfollowUser, login, logout, signup, updateUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../utils/verifyUser.js';
+
 
 const router = express.Router();
 
 router.post('/signup', signup);
 router.post('/login', login);
+router.post('/logout', logout);
+router.post('/follow/:id', verifyToken, followUnfollowUser);
+router.post('/update/:id', verifyToken, updateUser);
 
 export default router;
