611b59782c570995b95cda03540b688a56d8f1f4
itzhimangshu <itzhimangshu@gmail.com>
2023-12-05 19:19:49 +0530

    follow/unfollow

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,14 +1,59 @@
+import { useEffect, useState } from "react";
 import UserHeader from "../components/UserHeader";
-import UserPost from "../components/UserPost";
+import { useParams } from "react-router-dom";
+import useShowToast from "../hooks/useShowToast";
 
 const UserPage = () => {
+  const { user, loading } = useGetUserProfile();
+  const { username } = useParams();
+  const showToast = useShowToast();
+	const [posts, setPosts] = useRecoilState(postsAtom);
+	const [fetchingPosts, setFetchingPosts] = useState(true);
+
+  useEffect(() => {
+    const getPosts = async () => {
+      if (!user) return;
+      setFetchingPosts(true);
+      try {
+        const res = await fetch(`/api/posts/user/${username}`);
+        const data = await res.json();
+        console.log(data);
+        setPosts(data);
+      } catch (error) {
+        showToast("Error", error.message, "error");
+        setPosts([]);
+      } finally {
+        setFetchingPosts(false);
+      }
+    };
+
+    getPosts();
+  }, [username, showToast, setPosts, user]);
+
+  if (!user && loading) {
+		return (
+			<Flex justifyContent={"center"}>
+				<Spinner size={"xl"} />
+			</Flex>
+		);
+	}
+
+	if (!user && !loading) return <h1>User not found</h1>;
+  
   return (
     <>
-      <UserHeader />
-      <UserPost likes={1200} replies={481} postImg="/post1.png" postTitle="Let's talk about threads." />
-      <UserPost likes={451} replies={12} postImg="/post2.png" postTitle="Nice tutorial." />
-      <UserPost likes={321} replies={989} postImg="/post3.png" postTitle="I love this guy." />
-      <UserPost likes={212} replies={56} postTitle="This is my first thread." />
+      <UserHeader user={user} />
+
+      {!fetchingPosts && posts.length === 0 && <h1>User has not posts.</h1>}
+      {fetchingPosts && (
+        <Flex justifyContent={"center"} my={12}>
+          <Spinner size={"xl"} />
+        </Flex>
+      )}
+
+      {posts.map((post) => (
+        <Post key={post._id} post={post} postedBy={post.postedBy} />
+      ))}
     </>
   );
 };
